db8b9085864d667f0f050d0ce9a2a0e8fb07cca2
8888Kar8888 <tashchyankar@gmail.com>
2024-03-28 17:25:13 +0400

    WIP: maven test 1

---
+++ src/main/java/com/example/capstone_maven/CapstoneMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstone_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/capstone_maven/CapstoneMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
